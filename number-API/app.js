ead7fc9638e7d2107c98bff7c3e196db323ab4c3
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2022-06-14 17:32:20 +0300

    numberAPI added

---
+++ number-API/app.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("button");
+let result = document.getElementById("number");
+
+
+button.addEventListener("click",() => {
+    let finalURL = `http://numbersapi.com/number/type`;
+
+    fetch(finalURL).
+})
